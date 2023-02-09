3d22f8ec02f77b40a455b1864490f1509615aad0
Zeeek250 <damonbaker27@hotmail.com>
2023-02-09 15:25:25 -0600

    added logged in user obervable

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -1,14 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-
   baseUrl = 'https://localhost:7296/api/';
+
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
@@ -17,14 +19,21 @@ export class AccountService {
       map((response: User)=>{
         const user = response;
         if(user){
-          localStorage.setItem('user', JSON.stringify(user))
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
         }
       } )
     );
   }
 
+  setCurrentUser(user :User){
+    this.currentUserSource.next(user);
+  }
+
+
   logout(){
     localStorage.removeItem('user');
+    this.currentUserSource.next(null);
   }
 
 
